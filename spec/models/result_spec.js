a52b1b9abba021035865feed74eaf2c8d99e4c7f
holmesie <aholmes@mitre.org>
2018-03-01 13:21:18 -0500

    Fixed linting issues; Adjusted patientSource usage

--- spec/models/result_spec.js
+++ spec/models/result_spec.js
@@ -3,7 +3,7 @@
 var Result = require('../../lib/models/result.js');
 
 describe("A result", function() {
-  let connectionInfo = "mongodb://127.0.0.1:27017/js-ecqme-test";
+  const connectionInfo = "mongodb://127.0.0.1:27017/js-ecqme-test";
   let result = new Result(connectionInfo);
 
   it("wraps up result storage to the db, both indidivual and aggregate", function() {
