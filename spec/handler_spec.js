25212d24db1eed59aba386c25996d34e1ca267c5
Louis Ades <lades@mitre.org>
2018-03-01 15:14:52 -0500

    Rebase with code_quality branch updates, lint new files, add Jasmine to eslint environment, add spec files to linting scope

--- spec/handler_spec.js
+++ spec/handler_spec.js
@@ -1,33 +1,32 @@
-var Handler = require('../lib/handler.js');
-var Result = require('../lib/models/result.js');
+const Handler = require('../lib/handler.js');
+const Result = require('../lib/models/result.js');
 
-describe("A handler", function() {
-  let connectionInfo = "mongodb://127.0.0.1:27017/js-ecqme-test";
-  let handler = new Handler(connectionInfo);
+describe('A handler', () => {
+  const connectionInfo = 'mongodb://127.0.0.1:27017/js-ecqme-test';
+  const handler = new Handler(connectionInfo);
 
-  it("can initialize a MongoDB connection", function() {
+  it('can initialize a MongoDB connection', () => {
     expect(handler.start()).toBe(true);
   });
 
-  it("stores and keeps track of calculation results by patient and by measure id", function() {
-    let dummy_patient = { id: 12345 };
+  it('stores and keeps track of calculation results by patient and by measure id', () => {
+    const dummyPatient = { id: 12345 };
 
-    let dummy_result = new Result(connectionInfo);
-    dummy_result.embedded.test_id = '11111';
-    dummy_result.embedded.measure_id = '54321';
+    const dummyResult = new Result(connectionInfo);
+    dummyResult.embedded.test_id = '11111';
+    dummyResult.embedded.measure_id = '54321';
 
-    handler.handleResult(dummy_patient, dummy_result);
+    handler.handleResult(dummyPatient, dummyResult);
     expect(handler.calculation_results[12345]['54321'].embedded.test_id).toBe('11111');
   });
 
-  it("wraps up result storage to the db, both indidivual and aggregate", function() {
-    let dummy_patient = { id: 12345 };
-    let dummy_result = new Result(connectionInfo);
-    dummy_result.embedded.test_id = '11111';
-    dummy_result.embedded.measure_id = '54321';
+  it('wraps up result storage to the db, both indidivual and aggregate', () => {
+    const dummyPatient = { id: 12345 };
+    const dummyResult = new Result(connectionInfo);
+    dummyResult.embedded.test_id = '11111';
+    dummyResult.embedded.measure_id = '54321';
 
-    handler.handleResult(dummy_patient, dummy_result);
-    
+    handler.handleResult(dummyPatient, dummyResult);
     expect(handler.finish()).toBe(true);
-  })
+  });
 });
--- spec/models/result_spec.js
+++ spec/models/result_spec.js
@@ -1,14 +1,14 @@
 const Result = require('../../lib/models/result.js');
 
-describe('A result', function() {
-  const connectionInfo = "mongodb://127.0.0.1:27017/js-ecqme-test";
-  let result = new Result(connectionInfo);
+describe('A result', () => {
+  const connectionInfo = 'mongodb://127.0.0.1:27017/js-ecqme-test';
+  const result = new Result(connectionInfo);
 
-  it("wraps up result storage to the db, both indidivual and aggregate", function() {
-    let dummy_result = new Result(connectionInfo);
-    dummy_result.embedded.test_id = '11111';
-    dummy_result.embedded.measure_id = '54321';
+  it('wraps up result storage to the db, both indidivual and aggregate', () => {
+    const dummyResult = new Result(connectionInfo);
+    dummyResult.embedded.test_id = '11111';
+    dummyResult.embedded.measure_id = '54321';
 
     expect(result.save()).toBe(true);
-  })
+  });
 });
