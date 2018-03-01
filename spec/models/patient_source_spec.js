c0d5a97838404fae44334a7314cdbf0e023e4a62
Louis Ades <lades@mitre.org>
2018-03-01 15:14:52 -0500

    Rebase with code_quality branch updates, lint new files, add Jasmine to eslint environment, add spec files to linting scope

--- spec/models/patient_source_spec.js
+++ spec/models/patient_source_spec.js
@@ -1,64 +1,59 @@
-"use strict" ;
+const PatientSource = require('../../lib/models/patient_source.js');
 
-var PatientSource = require('../../lib/models/patient_source.js');
+describe('A MongoDB Patient Source', () => {
+  const connectionInfo = 'mongodb://127.0.0.1:27017/js-ecqme-test';
+  const patientSource = new PatientSource(connectionInfo);
 
-describe("A MongoDB Patient Soure", function() {
-  let connectionInfo = "mongodb://127.0.0.1:27017/js-ecqme-test";
-  let patient_source = new PatientSource(connectionInfo);
-
-  it("iterates through and returns a stored patient list", function() {
-    patient_source.patients = [
+  it('iterates through and returns a stored patient list', () => {
+    patientSource.patients = [
       { _id: '123456', given_names: ['A', 'B'], family_name: 'C' },
       { _id: '654456', given_names: ['C', 'B'], family_name: 'A' },
     ];
 
-    patient_source.reset();
-    let patient_id_1 = patient_source.next()._id;
-    let patient_id_2 = patient_source.next()._id;
+    patientSource.reset();
+    const patientId1 = patientSource.next().family_name;
+    const patientId2 = patientSource.next().family_name;
 
-    expect(patient_id_1).not.toBe(patient_id_2);
+    expect(patientId1).not.toBe(patientId2);
 
-    let patient_3 = patient_source.next();
+    const patient3 = patientSource.next();
 
-    expect(patient_3).toBeNull();
+    expect(patient3).toBeNull();
   });
 
-  it("iterates through then resets position in a stored patient list", function() {
-    patient_source.patients = [
+  it('iterates through then resets position in a stored patient list', () => {
+    patientSource.patients = [
       { _id: '123456', given_names: ['A', 'B'], family_name: 'C' },
       { _id: '654456', given_names: ['C', 'B'], family_name: 'A' },
     ];
 
-    patient_source.reset();
-    let patient_id_1 = patient_source.next().id;
+    patientSource.reset();
+    const patientId1 = patientSource.next().id;
 
-    let patient_2 = patient_source.next();
-    while (patient_2 != null) {
-      patient_2 = patient_source.next();
+    let patient2 = patientSource.next();
+    while (patient2 != null) {
+      patient2 = patientSource.next();
     }
 
-    patient_source.reset();
-    let patient_id_2 = patient_source.next().id;
+    patientSource.reset();
+    const patientId2 = patientSource.next().id;
 
-    expect(patient_id_1).toBe(patient_id_2);
+    expect(patientId1).toBe(patientId2);
   });
 
-  it("gets a list of MongoDB patients by one or more patient IDs", function() {
-    let patient_mongo = patient_source.QDMPatient({ given_names: ['A', 'B'], family_name: 'C' });
+  it('gets a list of MongoDB patients by one or more patient IDs', () => {
+    const patientMongo = patientSource.QDMPatient({ given_names: ['A', 'B'], family_name: 'C' });
 
-    let patient_id_1 = patient_mongo.id;
+    const patientId1 = patientMongo.id;
 
-    patient_mongo.save(function (err, doc) {
-      if (err) return console.error(err);
+    patientMongo.save((err) => {
+      if (err) return Error(err);
 
-      patient_source.findPatients(patient_id_1, function(patient_source) {
-        if (err) return console.error(err);
-        
-        expect(patient_source.next().family_name).toBe('C');
-        expect(patient_source.next()).toBeNull();
+      patientSource.findPatients(patientId1, (self) => {
+        expect(self.next().family_name).toBe('C');
+        expect(self.next()).toBeNull();
       });
-
+      return null;
     });
-
   });
 });
