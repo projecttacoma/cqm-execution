c2c7a593eb50d3da17dbc104091b059166d69d3d
Louis Ades <lades@mitre.org>
2018-04-26 19:24:54 -0400

    Resolve Sinon issues

--- spec/models/value_set_source_spec.js
+++ spec/models/value_set_source_spec.js
@@ -1,5 +1,4 @@
 const ValueSetSource = require('../../lib/models/value_set_source.js');
-const ValueSet = require('cqm-models').ValueSet;
 const Measure = require('cqm-models').Measure;
 const Mongoose = require('mongoose');
 const Sinon = require('sinon');
@@ -10,12 +9,12 @@ const ObjectId = Mongoose.Types.ObjectId;
 describe('A MongoDB ValueSet Source', () => {
   const connectionInfo = 'mongodb://127.0.0.1:27017/js-ecqme-test';
   const valueSetSource = new ValueSetSource(connectionInfo);
-  const vs1 = new ValueSet({
+  const vs1 = valueSetSource.ValueSet({
     oid: '12345-67890-cba',
     version: '',
     display_name: 'Test Valueset A',
   });
-  const vs2 = new ValueSet({
+  const vs2 = valueSetSource.ValueSet({
     oid: '1122334455-6677889900-abc',
     version: '',
     display_name: 'Test Valueset B',
@@ -29,28 +28,35 @@ describe('A MongoDB ValueSet Source', () => {
     value_sets: [ObjectId(vs2.id)],
   });
 
-  const mock = Sinon.mock(valueSetSource.ValueSet).expects('find').exactly(2);
+  const mock = Sinon.mock(valueSetSource.ValueSet);
+  let mockExpects;
 
+  beforeAll(() => {
+    mockExpects = mock.expects('find').exactly(2);
+  });
+  afterAll(() => {
+    mock.restore();
+  });
   describe('Finding a value set given a measure', () => {
     it('returns a list of value sets', () => {
-      mock.yields(null, [vs1]);
+      mockExpects.yields(null, [vs1]);
 
       Promise.resolve(valueSetSource.findValueSetsByMongoidForMeasures(mes1))
         .then((response) => {
-          expect(response[0]).toBe(vs1);
-          expect(valueSetSource.valueSetsByMongoid[vs1.id]).toBe(vs1);
+          expect(response[0].display_name).toBe(vs1.display_name);
+          expect(valueSetSource.valueSetsByMongoid[vs1._id].oid).toBe(vs1.oid);
         });
     });
   });
 
   describe('Finding a value set given a list of measures', () => {
     it('returns a list of value sets', () => {
-      mock.yields(null, [vs1, vs2]);
+      mockExpects.yields(null, [vs1, vs2]);
 
       Promise.resolve(valueSetSource.findValueSetsByMongoidForMeasures([mes1, mes2]))
         .then((response) => {
           expect(response.length).toBe(2);
-          expect(valueSetSource.valueSetsByMongoid[vs2.id]).toBe(vs2);
+          expect(valueSetSource.valueSetsByMongoid[vs2._id].oid).toBe(vs2.oid);
         });
     });
   });
