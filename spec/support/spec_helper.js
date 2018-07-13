ee505859d9ced643af1028cfaeea7cb4c97a03c1
Matt Mayer <mmayer@mitre.org>
2018-07-13 14:31:58 -0400

    Fix lint errors in spec_helper

--- spec/support/spec_helper.js
+++ spec/support/spec_helper.js
@@ -4,14 +4,14 @@ const path = require('path');
 const basePath = path.join('spec', 'fixtures', 'json');
 
 module.exports.getJSONFixture = (fixturePath) => {
-  var contents = fs.readFileSync(path.join(basePath, fixturePath));
+  const contents = fs.readFileSync(path.join(basePath, fixturePath));
   return JSON.parse(contents);
-}
+};
 
 module.exports.getEpisodeResults = (episodeResults) => {
-  results = [];
-  Object.keys(episodeResults).forEach( function(result) {
+  let results = [];
+  Object.keys(episodeResults).forEach((result) => {
     results = episodeResults[result].values.concat(results);
   });
   return results;
-}
+};
