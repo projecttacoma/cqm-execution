f28e4ac66dfed70b3384472470bc0bf40f1017fc
Matt Mayer <mmayer@mitre.org>
2018-07-13 16:49:59 -0400

    Address lint errors in measure_helpers_spec

--- spec/helpers/measure_helpers_spec.js
+++ spec/helpers/measure_helpers_spec.js
@@ -1,9 +1,9 @@
 const MeasureHelpers = require('../../lib/helpers/measure_helpers');
 const getJSONFixture = require('../support/spec_helper.js').getJSONFixture;
 
-describe('MeasureHelpers', function() {
-  describe('findAllLocalIdsInStatementByName', function() {
-    it('finds localIds for library FunctionRefs while finding localIds in statements', function() {
+describe('MeasureHelpers', () => {
+  describe('findAllLocalIdsInStatementByName', () => {
+    it('finds localIds for library FunctionRefs while finding localIds in statements', () => {
       // Loads Anticoagulation Therapy for Atrial Fibrillation/Flutter measure.
       // This measure has the MAT global functions library included and the measure uses the
       // "CalendarAgeInYearsAt" function.
@@ -16,10 +16,10 @@ describe('MeasureHelpers', function() {
 
       // For the fixture loaded for this test it is known that the library reference is 49 and the functionRef itself is 55.
       expect(localIds[49]).not.toBeUndefined();
-      expect(localIds[49]).toEqual({localId: '49', sourceLocalId: '55'});
+      expect(localIds[49]).toEqual({ localId: '49', sourceLocalId: '55' });
     });
 
-    it('finds localIds for library ExpressionRefs while finding localIds in statements', function() {
+    it('finds localIds for library ExpressionRefs while finding localIds in statements', () => {
       // Loads Test104 aka. CMS13 measure.
       // This measure has both the TJC_Overall and MAT global libraries
       const measure = getJSONFixture('measures/CMS13v2/CMS13v2.json');
@@ -31,10 +31,10 @@ describe('MeasureHelpers', function() {
 
       // For the fixture loaded for this test it is known that the library reference is 109 and the functionRef itself is 110.
       expect(localIds[109]).not.toBeUndefined();
-      expect(localIds[109]).toEqual({localId: '109', sourceLocalId: '110'});
+      expect(localIds[109]).toEqual({ localId: '109', sourceLocalId: '110' });
     });
 
-    it('handles library ExpressionRefs with libraryRef embedded in the clause', function() {
+    it('handles library ExpressionRefs with libraryRef embedded in the clause', () => {
       // Loads Test104 aka. CMS13 measure.
       // This measure has both the TJC_Overall and MAT global libraries
       const measure = getJSONFixture('measures/CMS13v2/CMS13v2.json');
@@ -46,13 +46,13 @@ describe('MeasureHelpers', function() {
 
       // For the fixture loaded for this test it is known that the library reference is 109 and the functionRef itself is 110.
       expect(localIds[42]).not.toBeUndefined();
-      expect(localIds[42]).toEqual({localId: '42'});
+      expect(localIds[42]).toEqual({ localId: '42' });
     });
   });
 
 
-  describe('findLocalIdForLibraryRef for functionRefs', function() {
-    beforeEach(function() {
+  describe('findLocalIdForLibraryRef for functionRefs', () => {
+    beforeEach(() => {
       // Use a chunk of this fixture for these tests.
       const measure = getJSONFixture('measures/CMS723v0/CMS723v0.json');
 
@@ -61,34 +61,34 @@ describe('MeasureHelpers', function() {
       this.annotationSnippet = measure.elm[0].library.statements.def[6].annotation;
     });
 
-    it('returns correct localId for functionRef if when found', function() {
+    it('returns correct localId for functionRef if when found', () => {
       const ret = MeasureHelpers.findLocalIdForLibraryRef(this.annotationSnippet, '55', 'global');
       expect(ret).toEqual('49');
     });
 
-    it('returns null if it does not find the localId for the functionRef', function() {
+    it('returns null if it does not find the localId for the functionRef', () => {
       const ret = MeasureHelpers.findLocalIdForLibraryRef(this.annotationSnippet, '23', 'global');
       expect(ret).toBeNull();
     });
 
-    it('returns null if it does not find the proper libraryName for the functionRef', function() {
+    it('returns null if it does not find the proper libraryName for the functionRef', () => {
       const ret = MeasureHelpers.findLocalIdForLibraryRef(this.annotationSnippet, '55', 'notGlobal');
       expect(ret).toBeNull();
     });
 
-    it('returns null if annotation is empty', function() {
+    it('returns null if annotation is empty', () => {
       const ret = MeasureHelpers.findLocalIdForLibraryRef({}, '55', 'notGlobal');
       expect(ret).toBeNull();
     });
 
-    it('returns null if there is no value associated with annotation', function() {
+    it('returns null if there is no value associated with annotation', () => {
       const ret = MeasureHelpers.findLocalIdForLibraryRef(this.annotationSnippet, '68', 'notGlobal');
       expect(ret).toBeNull();
     });
   });
 
-  describe('findLocalIdForLibraryRef for expressionRefs', function() {
-    beforeEach(function() {
+  describe('findLocalIdForLibraryRef for expressionRefs', () => {
+    beforeEach(() => {
       // Use a chunk of this fixture for these tests.
       const measure = getJSONFixture('measures/CMS13v2/CMS13v2.json');
 
@@ -98,24 +98,24 @@ describe('MeasureHelpers', function() {
       this.annotationSnippet = measure.elm[0].library.statements.def[12].annotation;
     });
 
-    it('returns correct localId for expressionRef when found', function() {
+    it('returns correct localId for expressionRef when found', () => {
       const ret = MeasureHelpers.findLocalIdForLibraryRef(this.annotationSnippet, '110', 'TJC');
       expect(ret).toEqual('109');
     });
 
-    it('returns null if it does not find the localId for the expressionRef', function() {
+    it('returns null if it does not find the localId for the expressionRef', () => {
       const ret = MeasureHelpers.findLocalIdForLibraryRef(this.annotationSnippet, '21', 'TJC');
       expect(ret).toBeNull();
     });
 
-    it('returns null if it does not find the proper libraryName for the expressionRef', function() {
+    it('returns null if it does not find the proper libraryName for the expressionRef', () => {
       const ret = MeasureHelpers.findLocalIdForLibraryRef(this.annotationSnippet, '110', 'notTJC');
       expect(ret).toBeNull();
     });
   });
 
-  describe('findLocalIdForLibraryRef for expressionRefs with libraryRef in clause', function() {
-    beforeEach(function() {
+  describe('findLocalIdForLibraryRef for expressionRefs with libraryRef in clause', () => {
+    beforeEach(() => {
       // Use a chunk of this fixture for these tests.
       const measure = getJSONFixture('measures/CMS13v2/CMS13v2.json');
 
@@ -125,12 +125,12 @@ describe('MeasureHelpers', function() {
       this.annotationSnippet = measure.elm[0].library.statements.def[8].annotation;
     });
 
-    it('returns null for expressionRef when found yet it is embedded', function() {
+    it('returns null for expressionRef when found yet it is embedded', () => {
       const ret = MeasureHelpers.findLocalIdForLibraryRef(this.annotationSnippet, '42', 'TJC');
       expect(ret).toBeNull();
     });
 
-    it('returns null if it does not find the proper libraryName for the expressionRef', function() {
+    it('returns null if it does not find the proper libraryName for the expressionRef', () => {
       const ret = MeasureHelpers.findLocalIdForLibraryRef(this.annotationSnippet, '42', 'notTJC');
       expect(ret).toBeNull();
     });
