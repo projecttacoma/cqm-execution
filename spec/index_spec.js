0c98c89cd26e7a0b441cdbeeca38b2cd37c1cdcd
Luke Osborne <lwosborne@mitre.org>
2018-07-18 09:27:17 -0400

    add semicolons

--- spec/index_spec.js
+++ spec/index_spec.js
@@ -1,8 +1,8 @@
-const calculatorHelpers = require('../lib/index.js').CalculatorHelpers
-const measureHelpers = require('../lib/index.js').MeasureHelpers
-const resultsHelpers = require('../lib/index.js').ResultsHelpers
-const patientSource = require('../lib/index.js').PatientSource
-const calculator = require('../lib/index.js').Calculator
+const calculatorHelpers = require('../lib/index.js').CalculatorHelpers;
+const measureHelpers = require('../lib/index.js').MeasureHelpers;
+const resultsHelpers = require('../lib/index.js').ResultsHelpers;
+const patientSource = require('../lib/index.js').PatientSource;
+const calculator = require('../lib/index.js').Calculator;
 
 
 describe('Module exports', () => {
