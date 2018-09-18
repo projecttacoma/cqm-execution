6e259726ba17f7653160e1f08731d5bd3c602c4a
Matt Mayer <mmayer@mitre.org>
2018-09-18 11:38:47 -0400

    Add calculator_helpers spec

---
+++ spec/helpers/calculator_helpers_spec.js
@@ -0,0 +1,9 @@
+const CalculatorHelpers = require('../../lib/helpers/calculator_helpers');
+
+describe('CalculatorHelpers', () => {
+    it('gets STRAT index from stratification name', () => {
+        expect(CalculatorHelpers.getStratIndexFromStratName("STRAT")).toEqual(0);
+        expect(CalculatorHelpers.getStratIndexFromStratName("STRAT_1")).toEqual(1);
+        expect(CalculatorHelpers.getStratIndexFromStratName("STRAT_123")).toEqual(123);
+    });
+});
