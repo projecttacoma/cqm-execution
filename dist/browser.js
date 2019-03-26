93759f318b557f07aeaadf0a36598d17d8a0f3fe
Chris Hossenlopp <hossenlopp@mitre.org>
2019-03-26 07:49:26 -0400

    added cqm-models to brower.js exposing code

--- dist/browser.js
+++ dist/browser.js
@@ -1,8 +1,9 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 window.cqm = window.cqm || {};
 window.cqm.execution = require('./index');
+window.cqm.models = require('cqm-models');
 
-},{"./index":5}],2:[function(require,module,exports){
+},{"./index":5,"cqm-models":253}],2:[function(require,module,exports){
 const moment = require('moment');
 
 /**
