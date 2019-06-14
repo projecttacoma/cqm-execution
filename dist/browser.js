9e035bff720ab44be9171b97250e88611f5ba428
Chris Hossenlopp <hossenlopp@mitre.org>
2019-06-14 17:37:27 -0400

    updated yarn.lock and dist with casting update

--- dist/browser.js
+++ dist/browser.js
@@ -66093,6 +66093,7 @@ module.exports = DateTime;
 },{"cql-execution":38,"mongoose/browser":254}],235:[function(require,module,exports){
 const mongoose = require('mongoose/browser');
 const cql = require('cql-execution');
+const DateTime = require('./DateTime');
 
 function Interval(key, options) {
   mongoose.SchemaType.call(this, key, options, 'Interval');
@@ -66116,17 +66117,11 @@ Interval.prototype.cast = (interval) => {
 
   // Cast to DateTime if it is a string representing a DateTime
   if (casted.low) {
-    if (!Date.parse(casted.low)) {
-      throw new Error(`DateTime: ${casted.low} is not a valid DateTime`);
-    }
-    casted.low = cql.DateTime.fromJSDate(new Date(casted.low), 0);
+    casted.low = DateTime.prototype.cast(casted.low);
   }
 
   if (casted.high) {
-    if (!Date.parse(casted.high)) {
-      throw new Error(`DateTime: ${casted.high} is not a valid DateTime`);
-    }
-    casted.high = cql.DateTime.fromJSDate(new Date(casted.high), 0);
+    casted.high = DateTime.prototype.cast(casted.high);
   }
   return casted;
 };
@@ -66134,7 +66129,7 @@ Interval.prototype.cast = (interval) => {
 mongoose.Schema.Types.Interval = Interval;
 module.exports = Interval;
 
-},{"cql-execution":38,"mongoose/browser":254}],236:[function(require,module,exports){
+},{"./DateTime":234,"cql-execution":38,"mongoose/browser":254}],236:[function(require,module,exports){
 const mongoose = require('mongoose/browser');
 const cql = require('cql-execution');
 
