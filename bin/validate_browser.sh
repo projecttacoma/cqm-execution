689c09b00230693cf30d6618b657e577930211f0
Luke Osborne <lwosborne@mitre.org>
2019-07-15 11:45:12 -0400

    test

--- bin/validate_browser.sh
+++ bin/validate_browser.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 yarn run browser
-rm dist/browser.js-e
+# Remove a temporary file created by browserify if it exists
+rm -f dist/browser.js-e
 
 if git diff-index --quiet HEAD --; then
-  echo "dist/browser.js is up to date"
-  exit 0
-else
   echo "dist/browser.js is out of date. Please run 'yarn run browser' locally and commit/push the result"
   exit 1
+else
+  echo "dist/browser.js is up to date"
+  exit 0
 fi
 
