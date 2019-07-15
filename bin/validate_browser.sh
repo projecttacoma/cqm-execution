4beddeaeff6595996a6d6963ce3d305d58a6d9c9
Luke Osborne <lwosborne@mitre.org>
2019-07-15 10:16:25 -0400

    Update validate_browser.sh

--- bin/validate_browser.sh
+++ bin/validate_browser.sh
@@ -5,6 +5,10 @@ mkdir -p tmp/dist/
 # browser_test browserifies index.js to tmp/dist
 yarn run browser_test
 
+# Repeat the fix-mongoose.sh changes on the temp browser
+sed -i -e "s/case \'Date\'/case 'MongooseDate'/g" ./tmp/dist/browser.js
+sed -i -e "s/'Date' === obj\.constructor\.name/'MongooseDate' === obj\.constructor\.name/g" ./tmp/dist/browser.js
+
 # comm -3 only returns lines that differ between the two files. If none are different, diff will be empty
 diff=`diff dist/browser.js tmp/dist/browser.js`
 
