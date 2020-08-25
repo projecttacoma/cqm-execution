eae9865d7091b7d3f42806bae82cea6418f33750
Serhii Ilin <serhii.ilin@semanticbits.com>
2020-08-25 16:29:55 -0400

    MAT-1624 Added more unit tests for CMS74

--- spec/models/calculator.CMS74.spec.js
+++ spec/models/calculator.CMS74.spec.js
@@ -89,5 +89,63 @@ describe("Calculator", () => {
         NUMER: false,
       });
     });
+
+    it("calculates numer-strat2 patient", () => {
+      const measure = getJSONFixture("fhir_cqm_measures/CMS74/CMS74.json");
+      const valueSets = measure.value_sets;
+      const patients = [];
+      patients.push(
+        getJSONFixture("fhir_cqm_measures/CMS74/tests-numer-strat2-EXM74-cqm-patient.json")
+      );
+      const calculationResults = Calculator.calculate(
+        measure,
+        patients,
+        valueSets,
+        { }
+      );
+      const result = Object.values(
+        calculationResults[Object.keys(calculationResults)[0]]
+      )[0];
+
+      // There will not be episode_results on the result object
+      expect(result["episode_results"]).toBeUndefined();
+      // The IPP should be the only relevant population
+      // TODO: why not denomexcl?
+      expect(result.population_relevance).toEqual({
+        IPP: true,
+        DENOM: false,
+        DENEX: false,
+        NUMER: false,
+      });
+    });
+
+    it("calculates numer-strat3 patient", () => {
+      const measure = getJSONFixture("fhir_cqm_measures/CMS74/CMS74.json");
+      const valueSets = measure.value_sets;
+      const patients = [];
+      patients.push(
+        getJSONFixture("fhir_cqm_measures/CMS74/tests-numer-strat3-EXM74-cqm-patient.json")
+      );
+      const calculationResults = Calculator.calculate(
+        measure,
+        patients,
+        valueSets,
+        { }
+      );
+      const result = Object.values(
+        calculationResults[Object.keys(calculationResults)[0]]
+      )[0];
+
+      // There will not be episode_results on the result object
+      expect(result["episode_results"]).toBeUndefined();
+      // The IPP should be the only relevant population
+      // TODO: why not denomexcl?
+      expect(result.population_relevance).toEqual({
+        IPP: true,
+        DENOM: false,
+        DENEX: false,
+        NUMER: false,
+      });
+    });
   });
 });
