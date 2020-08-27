5a2f895f8af5345e7f58700cbf9dbe00a7cc9981
Serhii Ilin <serhii.ilin@semanticbits.com>
2020-08-27 10:58:52 -0400

    MAT-1624 added more unit tests to cqm-execution

---
+++ spec/models/calculator.CMS108.spec.js
@@ -0,0 +1,50 @@
+const Calculator = require("../../lib/models/calculator.js");
+const getJSONFixture = require("../support/spec_helper.js").getJSONFixture;
+
+describe("Calculator.CMS108", () => {
+  it("calculates patient1", () => {
+    const measure = getJSONFixture("fhir_cqm_measures/CMS108/CMS108.json");
+    const valueSets = measure.value_sets;
+    const patients = [];
+    patients.push(getJSONFixture("fhir_cqm_measures/CMS108/tests-patient1-CMS108-cqm-patient.json"));
+    const calculationResults = Calculator.calculate(measure, patients, valueSets, {});
+    expect(Object.keys(calculationResults).length).toEqual(1);
+    const result = Object.values(calculationResults[Object.keys(calculationResults)[0]])[0];
+
+    // There will not be episode_results on the result object
+    expect(result["episode_results"]).toEqual({});
+    expect(result.population_relevance).toEqual({
+      IPP: true,
+      DENEX: false,
+      DENOM: false,
+      NUMER: false
+    });
+    expect(result.clause_results).toBeNull();
+    expect(result.state).toEqual("complete");
+    expect(result.IPP).toEqual(0);
+    expect(result.patient).toEqual("patient1-CMS108");
+  });
+
+  it("calculates patient2", () => {
+    const measure = getJSONFixture("fhir_cqm_measures/CMS108/CMS108.json");
+    const valueSets = measure.value_sets;
+    const patients = [];
+    patients.push(getJSONFixture("fhir_cqm_measures/CMS108/tests-patient2-CMS108-cqm-patient.json"));
+    const calculationResults = Calculator.calculate(measure, patients, valueSets, {});
+    expect(Object.keys(calculationResults).length).toEqual(1);
+    const result = Object.values(calculationResults[Object.keys(calculationResults)[0]])[0];
+
+    // There will not be episode_results on the result object
+    expect(result["episode_results"]).toEqual({});
+    expect(result.population_relevance).toEqual({
+      IPP: true,
+      DENEX: false,
+      DENOM: false,
+      NUMER: false,
+    });
+    expect(result.clause_results).toBeNull();
+    expect(result.state).toEqual("complete");
+    expect(result.IPP).toEqual(0);
+    expect(result.patient).toEqual("patient2-CMS108");
+  });
+});
